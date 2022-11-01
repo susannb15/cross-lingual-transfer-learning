cbc98f77a1b62302679bb572e47c6d7dc0633f6a
Susann Boy <susannb@jones-6.coli.uni-saarland.de>
2022-11-01 11:46:07 +0100

    test tiger sents

--- test_tiger.py
+++ test_tiger.py
@@ -21,51 +21,33 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelWithLMHead, AutoConfig, GPT2LMHeadModel
 
 tokenizer = AutoTokenizer.from_pretrained("dbmdz/german-gpt2")
-model = AutoModelWithLMHead.from_pretrained("dbmdz/german-gpt2")
 
-"""
-def tokenize_function(examples):
-	return tokenizer(examples["text"])
+native_model = AutoModelWithLMHead.from_pretrained("dbmdz/german-gpt2")
+de_model = AutoModelWithLMHead.from_pretrained("de_tied/checkpoint-100000")
+en_model = AutoModelWithLMHead.from_pretrained("en_tied/checkpoint-100000")
+es_model = AutoModelWithLMHead.from_pretrained("es_tied/checkpoint-100000")
 
-tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
-
-block_size=256
-
-def group_texts(examples):
-	# Concatenate all texts.
-	concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
-	total_length = len(concatenated_examples[list(examples.keys())[0]])
-	# We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-	# customize this part to your needs.
-	if total_length >= block_size:
-		total_length = (total_length // block_size) * block_size
-	# Split by chunks of max_len.
-	result = {
-		k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-		for k, t in concatenated_examples.items()
-		}
-	result["labels"] = result["input_ids"].copy()
-	return result
-
-lm_datasets = tokenized_datasets.map(
-	group_texts,
-	batched=True,
-	batch_size=1000,
-	num_proc=4,
-)
-"""
+models = [("native", native_model),("de", de_model),("en", en_model),("es", es_model)]
 
 import math
 from tqdm import tqdm
 
-with open("tiger.txt", "r", encoding='ISO-8859-1') as f:
+ppls = dict()
+
+#with open("tiger.txt", "r", encoding='ISO-8859-1') as f:
 	text = f.readlines()
-	ppl_total = 0
-	for line in tqdm(text): 
-		with torch.no_grad():
-			tokenized = tokenizer(line, return_tensors="pt")
-			outputs = model(**tokenized, labels=tokenized["input_ids"])
-			loss = outputs.loss
-			ppl_total += math.exp(loss)
-	print(f"AVG PPL: {ppl_total/len(text)}")
+	text = text[:10000]
+	for name, model in models:
+		ppl_total = 0
+		for line in tqdm(text): 
+			with torch.no_grad():
+				tokenized = tokenizer(line, return_tensors="pt")
+				outputs = model(**tokenized, labels=tokenized["input_ids"])
+				loss = outputs.loss
+				ppl_total += math.exp(loss)
+		ppls[name] = ppl_total / len(text)
+
+with open("ppls.txt", "w+") as f:
+	for key in ppls:
+		f.write("AVG PPL of "+str(key)+": "+str(ppls[key])+"\n")
 
