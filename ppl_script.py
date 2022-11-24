3ebc6b31995b4066decada8e4fc158cbb4b63732
Susann Boy <susannb@tony-1.coli.uni-saarland.de>
2022-11-24 14:48:35 +0100

    ppl_script.py

---
+++ ppl_script.py
@@ -0,0 +1,78 @@
+# A script for ppl output per word in a sentence
+
+import transformers
+from datasets import load_dataset
+import torch
+import torch.nn as nn
+import argparse
+import math
+from trainer_mod import My_Trainer
+import random
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelWithLMHead, AutoConfig, GPT2LMHeadModel
+import matplotlib.pyplot as plt
+
+random.seed(42)
+
+parser = argparse.ArgumentParser(description="Options")
+
+parser.add_argument('--data', help='Dataset to be tested. Should contain sample sentences')
+parser.add_argument('--model', default='from_scratch', help='Model to be tested. Specify: pre-trained, from_scratch, de, en, es')
+
+args = parser.parse_args()
+
+# load model and tokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("dbmdz/german-gpt2")
+
+if args.model == "pre-trained":
+	model = AutoModelWithLMHead.from_pretrained("dbmdz/german-gpt2")
+elif args.model == "de":
+	model = AutoModelWithLMHead.from_pretrained("de_tied/checkpoint-60000")
+elif args.model == "en":
+	model = AutoModelWithLMHead.from_pretrained("en_tied/checkpoint-60000")
+elif args.model == "es":
+	model = AutoModelWithLMHead.from_pretrained("es_tied/checkpoint-60000")
+else:
+	model = AutoModelWithLMHead.from_pretrained("de_from_scratch/checkpoint-45000")
+	print("No model specified or unknown model name. Taking DE model trained from scratch")
+
+def read_file(file_name):
+	with open(file_name, "r") as f:
+		text = f.readlines()
+	return text
+
+content = read_file(args.data)
+
+for sentence in content:
+	with torch.no_grad():
+		tokenized = tokenizer(sentence, return_tensors='pt')
+		labels = tokenized["input_ids"]
+		outputs = model(**tokenized, labels=labels)
+		logits = outputs.get("logits")
+		shift_logits = logits[..., :-1, :].contiguous()
+		shift_labels = labels[..., 1:].contiguous()
+		loss_fct = nn.CrossEntropyLoss(reduction="none")
+		loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+		print(loss)
+		print(loss.shape)
+
+"""
+for sentence in content:
+	words = sentence.split()
+	ppls = []
+	for i in range(1, len(words)+1):
+		print(i)
+		sentence_part = " ".join(words[:i])
+		print(sentence_part)
+		with torch.no_grad():
+			tokenized = tokenizer(sentence_part, return_tensors='pt')
+			print(tokenized)
+			outputs = model(**tokenized, labels=tokenized["input_ids"])
+			ppl = math.exp(outputs.loss)
+			ppls.append(outputs.loss)
+	print(words, ppls)
+	plt.plot(words, ppls)
+	plt.title(f"PPL per word for: {sentence}")
+	plt.show()
+		
+"""
