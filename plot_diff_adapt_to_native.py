8a57d0ee0b5d6d738074a8177defc8d01fd4d557
Susann Boy <susannb@tony-2.coli.uni-saarland.de>
2023-06-10 18:38:59 +0200

    added plot_diff_adapt_to_native

---
+++ plot_diff_adapt_to_native.py
@@ -0,0 +1,26 @@
+
+# Values of the native models
+ppl_native_de = 48.5
+ppl_netive_en = 51.5
+ppl_native_es = 53.7
+
+# get difference ppl(native) - ppl(adapted)
+# (ppl diff of X adapted on Y)
+ppl_de_de = ppl_native_de - 
+ppl_en_de = ppl_native_de -
+ppl_es_de = ppl_native_de -
+
+ppl_de_en = ppl_native_en -
+ppl_en_en = ppl_native_en -
+ppl_es_en = ppl_native_en -
+
+ppl_de_es = ppl_native_es -
+ppl_en_es = ppl_native_es -
+ppl_es_es = ppl_native_es -
+
+# Value packs in list DE-EN-ES
+ppls_de = [ppl_de_de, ppl_en_de, ppl_es_de]
+ppls_en = [ppl_de_en, ppl_en_en, ppl_es_en]
+ppls_es = [ppl_de_es, ppl_en_es, ppl_es_es]
+
+
